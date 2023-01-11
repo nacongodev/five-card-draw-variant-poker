a773c5e0caaf31d366431bd16317ff3b8fe0e4ac
Jean tsheta <tshieta@gmail.com>
2023-01-11 23:02:20 +0200

    Delete backend directory

--- backend/tests/test_card.py
+++
@@ -1,9 +0,0 @@
-from unittest import TestCase
-from card import Card
-
-
-class TestCard(TestCase):
-    def test_attributes(self):
-        card = Card('hearts', 1)
-        self.assertEqual(card.suit, 'hearts')
-        self.assertEqual(card.rank, 1)
