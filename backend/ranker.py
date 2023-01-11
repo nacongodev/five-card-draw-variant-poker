93b69f1da6f5d6ae9b2d57e6e3d9f6a269275631
Jean tsheta <tshieta@gmail.com>
2023-01-11 20:34:00 +0200

    Update ranker.py

--- backend/ranker.py
+++ backend/ranker.py
@@ -1,74 +1,92 @@
+from collections import Counter
 class Ranker:
     def __init__(self):
-        self.ranks = ['high card', 'pair', 'two pair', 'three of a kind', 'straight', 'flush', 'full house',
-                      'four of a kind', 'straight flush']
+        pass
 
     def rank_hand(self, hand):
-        ranks = []
-        for card in hand.cards:
-            ranks.append(card.rank)
+        hand_rank = self.evaluate_hand(hand)
+        return hand_rank
 
+    def evaluate_hand(self, hand):
         if self.is_straight_flush(hand):
-            return self.ranks[8]
+            return "Straight Flush"
         elif self.is_four_of_a_kind(hand):
-            return self.ranks[7]
+            return "Four of a Kind"
         elif self.is_full_house(hand):
-            return self.ranks[6]
+            return "Full House"
         elif self.is_flush(hand):
-            return self.ranks[5]
+            return "Flush"
         elif self.is_straight(hand):
-            return self.ranks[4]
+            return "Straight"
         elif self.is_three_of_a_kind(hand):
-            return self.ranks[3]
+            return "Three of a Kind"
         elif self.is_two_pair(hand):
-            return self.ranks[2]
+            return "Two Pair"
         elif self.is_pair(hand):
-            return self.ranks[1]
+            return "One Pair"
         else:
-            return self.ranks[0]
-
-    def is_pair(self, hand):
-        ranks = []
-        for card in hand.cards:
-            ranks.append(card.rank)
-        return len(set(ranks)) == 4
-
-    def is_two_pair(self, hand):
-        ranks = []
-        for card in hand.cards:
-            ranks.append(card.rank)
-        return len(set(ranks)) == 3
-
-    def is_three_of_a_kind(self, hand):
-        ranks = []
-        for card in hand.cards:
-            ranks.append(card.rank)
-        return len(set(ranks)) == 3
-
-    def is_straight(self, hand):
-        ranks = []
-        for card in hand.cards:
-            ranks.append(card.rank)
-        ranks.sort()
-        return ranks[-1] - ranks[0] == 4
-
-    def is_flush(self, hand):
-        suits = []
-        for card in hand.cards:
-            suits.append(card.suit)
-        return len(set(suits)) == 1
-
-    def is_full_house(self, hand):
-        ranks = []
-        for card in hand.cards:
-            ranks.append(card.rank)
-        return len(set(ranks)) == 2
-
-    def is_four_of_a_kind(self, hand):
-        ranks = []
-        for card in hand.cards:
-            ranks.append(card.rank)
-        return len(set(ranks)) == 2
+            return "High Cards"
 
     def is_straight_flush(self, hand):
-        return self.is_straight(hand) and self.is_flush(hand)
+        sorted_hand = sorted(hand, key=lambda x: x.rank)
+        if self.is_flush(hand) and self.is_straight(sorted_hand):
+            return True
+        return False
+
+    def is_four_of_a_kind(self, hand):
+        rank_counts = Counter(card.rank for card in hand)
+        for count in rank_counts.values():
+            if count == 4:
+                return True
+        return False
+
+    def is_full_house(self, hand):
+        rank_counts = Counter(card.rank for card in hand)
+        three_of_a_kind = False
+        two_of_a_kind = False
+        for count in rank_counts.values():
+            if count == 3:
+                three_of_a_kind = True
+            elif count == 2:
+                two_of_a_kind = True
+        return three_of_a_kind and two_of_a_kind
+
+    def is_flush(self, hand):
+        suit_counts = Counter(card.suit for card in hand)
+        for count in suit_counts.values():
+            if count == 5:
+                return True
+        return False
+
+    def is_straight(self, hand):
+        ranks = [card.rank for card in hand]
+        ranks_set = set(ranks)
+        if len(ranks_set) != 5:
+            return False
+        if max(ranks_set) - min(ranks_set) == 4:
+            return True
+        if ranks == [14, 2, 3, 4, 5]:
+            return True
+        return False
+
+    def is_three_of_a_kind(self, hand):
+        rank_counts = Counter(card.rank for card in hand)
+        for count in rank_counts.values():
+            if count == 3:
+                return True
+        return False
+
+    def is_two_pair(self, hand):
+        rank_counts = Counter(card.rank for card in hand)
+        num_pairs = 0
+        for count in rank_counts.values():
+            if count == 2:
+                num_pairs += 1
+        return num_pairs == 2
+
+    def is_pair(self, hand):
+        rank_counts = Counter(card.rank for card in hand)
+        for count in rank_counts.values():
+            if count == 2:
+                return True
+        return False
