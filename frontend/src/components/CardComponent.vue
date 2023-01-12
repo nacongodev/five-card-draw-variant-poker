b64ff468db7c4c9717fe1cc21e1e902e34275a36
Jean tsheta <tshieta@gmail.com>
2023-01-12 14:32:06 +0200

    Update CardComponent.vue

--- frontend/src/components/CardComponent.vue
+++ frontend/src/components/CardComponent.vue
@@ -3,9 +3,17 @@
     <label>Number of cards:</label>
     <input type="number" v-model.number="numCards">
     <button @click="drawCards">Draw Cards</button>
+    <div v-if="hand.length">
+      <p>Your Hand:</p>
+      <ul>
+        <li v-for="(card, index) in hand" :key="index " :style="{display: 'inline-block'}">{{ card.rank }}{{ card.suit }}</li>
+      </ul>
+      <p>Your rank: {{ rank}}</p>
+    </div>
+    <div v-else>
+      <p>No cards in hand</p>
+    </div>
     <p v-if="error">{{ error }}</p>
-    <p v-else>Your Hand: {{ hand.join(', ') }}</p>
-    <p>Your Rank: {{ rank }}</p>
   </div>
 </template>
 
@@ -25,12 +33,17 @@ export default {
     async drawCards() {
       try {
         const response = await axios.get(`http://localhost:8000/draw/${this.numCards}`)
-        this.hand = response.data.hand
-        console.log(response.data)
-        this.rank = response.data.rank
-        console.log(response.data)
+        this.hand = response.data.Hand
+        console.log(this.hand)
+        this.rank = response.data.Rank
+        console.log(this.rank)
       } catch (error) {
         this.error = error.response.data.error
+        if (error.response.status === 404) {
+      this.error = "Not enough cards in the deck";
+    } else {
+      this.error = "An error occurred";
+    }
       }
     }
   }
